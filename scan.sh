a332dbbf34cbb5302821f8170bd734057fa0d5f7
yjyjyjy <prowessyang@gmail.com>
2023-03-02 11:52:57 -0800

    log ip

--- scan.sh
+++ scan.sh
@@ -4,13 +4,14 @@ echo "$(date)"' 👀 >>> scan.sh' >> /home/ubuntu/log.txt
 id=`/bin/ec2metadata | awk '/instance-id/ {printf $2}'`
 ip=`/bin/ec2metadata | awk '/public-ipv4/ {printf $2}'`
 echo "$(date)"' 👀 instanceId: '"$id" >> /home/ubuntu/log.txt
+echo "$(date)"' 👀 publicIp: '"$ip" >> /home/ubuntu/log.txt
 URL=""
 while true
 do
   URL=`awk '/Running on public URL: / {printf $7}' tmpOutput.txt`
   if [ "$URL" != "" ]
   then
-    echo "$(date)"'👀 URL: '"$URL" >> /home/ubuntu/log.txt
+    echo "$(date)"' 👀 URL: '"$URL" >> /home/ubuntu/log.txt
     cmd=$(curl -d '{"instanceId":"'"$id"'", "gradioEndpoint":"'"$URL"'", "publicIp":"'"$ip"'"}' -H "Content-Type: application/json" -X POST https://a1-rouge.vercel.app/api/instance/register)
     break
   else
