d2a3411b5c9de87fd00ba6271094c156994b299b
yjyjyjy <prowessyang@gmail.com>
2023-03-02 03:22:49 -0800

    boot

--- bootScript.sh
+++ bootScript.sh
@@ -1,25 +1,12 @@
 #!/bin/bash
-cd /home/ubuntu/stable-diffusion-webui-docker
-docker compose --profile auto up --build > /home/ubuntu/tmpOutput.txt
-
-var=`cat tmpOut.txt | awk '/Running on public URL: / {printf $7}'`
-echo $var
-URL=""
-while true
+/bin/ec2metadata >meta.txt
+prefix="instance-id: "
+while IFS= read -r line
 do
-  URL=`cat tmpOutput.txt | awk '/Running on public URL: / {printf $7}'`
-  if [ "$URL" != "" ]
-  then
-    echo $URL
-    break
-  else
-    sleep 1
-  fi
-  # while IFS= read -r line
-  # do
-  #         if [[ "$line" == *"Running on public URL:"* ]]
-  #         then
-  #                 id=${line#"$prefix"}
-  #         fi
-  # done <meta
-done
+        if [[ "$line" == *"instance-id"* ]]
+        then
+                id=${line#"$prefix"}
+        fi
+done <meta.txt
+echo $id
+cmd=$(curl -d '{"instanceId":"$id"}' -H "Content-Type: application/json" -X POST https://a1-rouge.vercel.app/api/instance/register)
