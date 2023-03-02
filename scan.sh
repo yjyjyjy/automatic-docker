1810f3cdb857ec23c6866b5294fb1f5674a65028
yjyjyjy <prowessyang@gmail.com>
2023-03-02 04:22:14 -0800

    update

--- scan.sh
+++ scan.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+id=`/bin/ec2metadata | awk '/instance-id/ {printf $2}'`
 URL=""
 while true
 do
@@ -5,6 +7,7 @@ do
   if [ "$URL" != "" ]
   then
     echo $URL
+    cmd=$(curl -d '{"instanceId":"'"$id"'", "endpoint":"'"$URL"'"}' -H "Content-Type: application/json" -X POST https://a1-rouge.vercel.app/api/instance/register)
     break
   else
     sleep 1
