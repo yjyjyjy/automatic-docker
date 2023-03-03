fb48b0c652d8cc961d153d0e7241c8863f62fd02
yjyjyjy <prowessyang@gmail.com>
2023-03-03 00:01:33 -0800

    update curl

--- scan.sh
+++ scan.sh
@@ -12,7 +12,7 @@ do
   if [ "$URL" != "" ]
   then
     echo "$(date)"' 👀 URL: '"$URL" >> /home/ubuntu/log.txt
-    cmd=$(curl -d '{"instanceId":"'"$id"'", "gradioEndpoint":"'"$URL"'", "publicIp":"'"$ip"'"}' -H "Content-Type: application/json" -X POST https://a1-rouge.vercel.app/api/instance/register)
+    cmd=$(curl -d '{"gradioLink":"'"$URL"'", "ip":"'"$ip"'"}' -H "Content-Type: application/json" -X POST https://a1-rouge.vercel.app/api/instance/$id)
     break
   else
     sleep 1
