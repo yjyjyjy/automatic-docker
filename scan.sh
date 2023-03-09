d498989e2aafab45256e150cb7178a629416dea6
yjyjyjy <prowessyang@gmail.com>
2023-03-08 17:08:19 -0800

    more info

--- scan.sh
+++ scan.sh
@@ -3,8 +3,17 @@ cd /home/ubuntu/automatic-docker/
 echo "$(date)"' 👀 >>> scan.sh' >> /home/ubuntu/log.txt
 id=`/bin/ec2metadata | awk '/instance-id/ {printf $2}'`
 ip=`/bin/ec2metadata | awk '/public-ipv4/ {printf $2}'`
+storage=`aws ec2 describe-tags --filters Name=key,Values=storage Name=resource-id,Values=i-076116efb3d37ada4 | jq -r '.Tags[0].Value'`
+userId=`aws ec2 describe-tags --filters Name=key,Values=userId Name=resource-id,Values=i-076116efb3d37ada4 | jq -r '.Tags[0].Value'`
+stackId=`aws ec2 describe-tags --filters Name=key,Values=aws:cloudformation:stack-id Name=resource-id,Values=i-076116efb3d37ada4 | jq -r '.Tags[0].Value'`
+launchTempateId=`aws ec2 describe-tags --filters Name=key,Values=aws:ec2launchtemplate:id Name=resource-id,Values=i-076116efb3d37ada4 | jq -r '.Tags[0].Value'`
+
 echo "$(date)"' 👀 instanceId: '"$id" >> /home/ubuntu/log.txt
 echo "$(date)"' 👀 publicIp: '"$ip" >> /home/ubuntu/log.txt
+echo "$(date)"' 👀 userId: '"$userId" >> /home/ubuntu/log.txt
+echo "$(date)"' 👀 storage: '"$storage" >> /home/ubuntu/log.txt
+echo "$(date)"' 👀 stackId: '"$stackId" >> /home/ubuntu/log.txt
+echo "$(date)"' 👀 launchTempateId: '"$launchTempateId" >> /home/ubuntu/log.txt
 URL=""
 while true
 do
@@ -12,7 +21,7 @@ do
   if [ "$URL" != "" ]
   then
     echo "$(date)"' 👀 URL: '"$URL" >> /home/ubuntu/log.txt
-    cmd=$(curl -v -d '{"gradioLink":"'"$URL"'", "ip":"'"$ip"'"}' -H "Content-Type: application/json" -X PUT https://a1-rouge.vercel.app/api/instance/$id)
+    cmd=$(curl -v -d '{"gradioLink":"'"$URL"'", "ip":"'"$ip"'", "ip":"'"$ip"'", "userId":"'"$userId"'", "storage":"'"$storage"'", "stackId":"'"$stackId"'", "launchTempateId":"'"$launchTempateId"'"}' -H "Content-Type: application/json" -X PUT https://a1-rouge.vercel.app/api/instance/$id)
     break
   else
     sleep 1
