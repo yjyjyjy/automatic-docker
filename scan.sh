55dc18ee3ec9da425e5b55777cf7201e0e498ad1
yjyjyjy <prowessyang@gmail.com>
2023-03-08 17:30:42 -0800

    update

--- scan.sh
+++ scan.sh
@@ -6,14 +6,14 @@ ip=`/bin/ec2metadata | awk '/public-ipv4/ {printf $2}'`
 storage=`aws ec2 describe-tags --filters Name=key,Values=storage Name=resource-id,Values=i-076116efb3d37ada4 | jq -r '.Tags[0].Value'`
 userId=`aws ec2 describe-tags --filters Name=key,Values=userId Name=resource-id,Values=i-076116efb3d37ada4 | jq -r '.Tags[0].Value'`
 stackId=`aws ec2 describe-tags --filters Name=key,Values=aws:cloudformation:stack-id Name=resource-id,Values=i-076116efb3d37ada4 | jq -r '.Tags[0].Value'`
-launchTempateId=`aws ec2 describe-tags --filters Name=key,Values=aws:ec2launchtemplate:id Name=resource-id,Values=i-076116efb3d37ada4 | jq -r '.Tags[0].Value'`
+launchTemplateId=`aws ec2 describe-tags --filters Name=key,Values=aws:ec2launchtemplate:id Name=resource-id,Values=i-076116efb3d37ada4 | jq -r '.Tags[0].Value'`
 
 echo "$(date)"' 👀 instanceId: '"$id" >> /home/ubuntu/log.txt
 echo "$(date)"' 👀 publicIp: '"$ip" >> /home/ubuntu/log.txt
 echo "$(date)"' 👀 userId: '"$userId" >> /home/ubuntu/log.txt
 echo "$(date)"' 👀 storage: '"$storage" >> /home/ubuntu/log.txt
 echo "$(date)"' 👀 stackId: '"$stackId" >> /home/ubuntu/log.txt
-echo "$(date)"' 👀 launchTempateId: '"$launchTempateId" >> /home/ubuntu/log.txt
+echo "$(date)"' 👀 launchTemplateId: '"$launchTemplateId" >> /home/ubuntu/log.txt
 URL=""
 while true
 do
@@ -21,7 +21,7 @@ do
   if [ "$URL" != "" ]
   then
     echo "$(date)"' 👀 URL: '"$URL" >> /home/ubuntu/log.txt
-    cmd=$(curl -v -d '{"gradioLink":"'"$URL"'", "ip":"'"$ip"'", "ip":"'"$ip"'", "userId":"'"$userId"'", "storage":"'"$storage"'", "stackId":"'"$stackId"'", "launchTempateId":"'"$launchTempateId"'"}' -H "Content-Type: application/json" -X PUT https://a1-rouge.vercel.app/api/instance/$id)
+    cmd=$(curl -v -d '{"gradioLink":"'"$URL"'", "ip":"'"$ip"'", "ip":"'"$ip"'", "userId":"'"$userId"'", "storage":"'"$storage"'", "stackId":"'"$stackId"'", "launchTemplateId":"'"$launchTemplateId"'"}' -H "Content-Type: application/json" -X PUT https://a1-rouge.vercel.app/api/instance/$id)
     break
   else
     sleep 1
